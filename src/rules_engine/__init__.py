d2698da27153bc84f44f1a69891245d6f1a844a1
Lennart Fridén <lennart@devl.se>
2021-10-10 13:04:42 +0200

    Bump version to 1.3.0.

--- src/rules_engine/__init__.py
+++ src/rules_engine/__init__.py
@@ -1,5 +1,5 @@
 from .rules_engine import RulesEngine, Rule, Otherwise, NoAction, then, when
 
-VERSION = "1.2.0"
+VERSION = "1.3.0"
 
 __all__ = [RulesEngine, Rule, Otherwise, NoAction, then, when]
